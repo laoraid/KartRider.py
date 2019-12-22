9989f516b2375a0488bcb873912c13f8ca016f5d
Laoraid <a99azaz@gmail.com>
2019-12-23 03:18:11 +0900

    docs 버전 변경

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Laoraid'
 author = 'Laoraid'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.3'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
